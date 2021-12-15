6974abb517ce631aa99115ed5c7352ba6e48c165
someh <someh2705@aiaracorp.com>
2021-12-15 18:35:12 +0900

    feat: Recomposition and Stable example code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,70 +1,76 @@
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.sharp.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 fun main() = Window {
-    MainWindow(rememberMainWindowState())
+    MainWindow(MainViewModel())
 }
 
 @Composable
-fun MainWindow(state: MainWindowState) {
+fun MainWindow(viewModel: MainViewModel) {
+    Log.d("Test", "Start")
+    val state by viewModel.counterState.collectAsState()
     Scaffold(
         topBar = {
             TopAppBar {
+                Log.d("Test", "TopAppBar Start")
                 Text(
-                    "Compose Demo Home Page",
+                    state.title,
                     style = MaterialTheme.typography.h6,
                     modifier = Modifier.padding(start = 16.dp)
                 )
             }
         }
     ) {
-        val count by state.count.collectAsState()
+        Counter(
+            state = state,
+            increase = { viewModel.increase() }
+        )
+    }
+}
 
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("You have pushed the button this many times:")
-                Text("$count", style = MaterialTheme.typography.h4)
-            }
-        }
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
-            IconButton(
-                modifier = Modifier.padding(end = 30.dp, bottom = 30.dp).size(60.dp).clip(CircleShape)
-                    .background(color = MaterialTheme.colors.primary),
-                onClick = state::increase
-            ) {
-                Icon(Icons.Sharp.Add, "Plus", tint = Color.White)
-            }
+data class CounterState(
+    val title: String,
+    val count: Int
+)
+
+@Composable
+fun Counter(
+    state: CounterState,
+    increase: () -> Unit,
+) {
+    Log.d("Test", "Counter Start")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("${state.count}", style = MaterialTheme.typography.h4)
+        Button(onClick = increase) {
+            Text("Button")
         }
     }
 }
 
-@Composable
-fun rememberMainWindowState() = remember {
-    MainWindowState()
-}
-
-class MainWindowState {
-    private val _count = MutableStateFlow(
-        0
-    )
-    val count: StateFlow<Int> = _count
+class MainViewModel {
+    private val _counterState = MutableStateFlow(CounterState(title = "Compose", count = 0))
+    val counterState: StateFlow<CounterState> = _counterState
 
     fun increase() {
-        _count.value++
+        val currentState = _counterState.value
+        _counterState.value = currentState.copy(
+            count = currentState.count + 1
+        )
     }
 }
